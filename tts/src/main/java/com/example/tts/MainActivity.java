9e05f83ccf19fb989e42ba1cd211077656d2eed7
Jasper-XHM <2298341583@qq.com>
2022-03-03 16:39:28 +0800

    Initial commit

---
+++ tts/src/main/java/com/example/tts/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.tts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private TextToSpeech textToSpeech;
+    private EditText editText;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+
+         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i!=TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.CHINA);
+                }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                Toast.makeText(MainActivity.this, ""+s, Toast.LENGTH_SHORT).show();
+                textToSpeech.speak(s,TextToSpeech.QUEUE_FLUSH,null);
+
+            }
+        });
+    }
+    public void onPause() {
+        if (textToSpeech!=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
+}
